f8bb56d69e4225f15b22aa0d5abfb78d447814f0
marcecabelloherrera <marcecabello.h@gmail.com>
2017-05-17 13:28:38 -0400

    desarrollo css y js

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+var elInput = document.getElementsByClassName("inpuT");
+var crearInput = document.createElement("input");
+var mensajeInput = document.createTextNode("");
+crearInput.appendChild(mensajeInput);
+elInput.appendChild(crearInput);
+
+var elBoton = document.getElementsByClassName("boton");
+var crearBoton = document.createElement("button");
+var atributoBoton = document.createAttribute("button");
+crearBoton.appendChild(atributoBoton);
+elBoton.appendChild(crearInput);
+
+function demo(){
+	var pulsar = document.getElementsByClassName("añade");
+	pulsar.addEventListener("click", function(){
+		elInput.appendChild(crearInput).toggle();
+	})
+}
